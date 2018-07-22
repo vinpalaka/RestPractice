6d0d17550ae0b47b7c876cebae52275f18fdaad7
Vin Palakkattil <vin.palakkattil@collectivehealth.com>
2018-07-22 12:34:27 -0700

    Adding update and backend map

--- src/main/java/spring_app/dao/DataAcessComp2.java
+++ src/main/java/spring_app/dao/DataAcessComp2.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Component//THis will get picked up when the other one is deleted or @component for
 //other class is commented out.
@@ -12,5 +13,13 @@ public class DataAcessComp2 implements IDataAcessComp {
     @Override
     public List<String> getNames() {
         return new ArrayList<>(Arrays.asList("Joseph", "Time"));
+    }
+
+    @Override public Map<Integer, String> update(int id, String value) {
+        return null;
+    }
+
+    @Override public Map<Integer, String> addName(int id, String value) {
+        return null;
     }
 }
