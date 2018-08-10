256e2055579b96e7047e07f58ff7e7be9e838f56
Vinayak Palakkattil <vinayakpalakkattil@Vinayaks-MacBook-Pro.local>
2018-08-09 19:12:22 -0700

    Adding create

--- src/main/java/spring_app/dao/DataAcessComp.java
+++ src/main/java/spring_app/dao/DataAcessComp.java
@@ -3,6 +3,7 @@ package spring_app.dao;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Component()
 public class DataAcessComp implements IDataAcessComp {
@@ -16,15 +17,17 @@ public class DataAcessComp implements IDataAcessComp {
 
     }
     public List<String> getNames() {
-        List<String> s = new ArrayList<>();
-        for(Integer i : map.keySet()) {
-            s.add(map.get(i));
-        }
-        return s;
+        return new ArrayList<>(map.values());
     }
 
     public void update(Integer i, String value) {
         if(map.containsKey(i)) {
+            map.put(i, value);
+        }
+    }
+
+    public void add(Integer i, String value) {
+        if(!map.containsKey(i)) {
             map.put(i, value);
         }
     }
